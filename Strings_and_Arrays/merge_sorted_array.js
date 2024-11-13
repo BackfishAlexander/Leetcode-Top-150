66977c46611f809ef45621a1bd20c78684ed3fdf
alex <backfish.alexander@gmail.com>
2024-11-12 22:54:04 -0500

    Added a better solution in O(m+n)

--- Strings_and_Arrays/merge_sorted_array.js
+++ Strings_and_Arrays/merge_sorted_array.js
@@ -1,8 +1,9 @@
 // Difficulty: Easy
 // Problemset: https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
-// Time complexity:     O(nlogn)
-// Space complexity:    O(1)
 
+// Solution 1:
+// Time complexity:     O((m+n)log(m+n)) *.sort() is very inefficient, but this problem isn't about clever sorting algorithms
+// Space complexity:    O(1)
 /**
  * @param {number[]} nums1
  * @param {number} m
@@ -15,4 +16,29 @@ var merge = function (nums1, m, nums2, n) {
     nums1[i] = nums2[i - m];
   }
   nums1.sort();
+};
+
+
+// Solution 2:
+// Time complexity:     O(m+n)
+// Space complexity:    O(1)
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+  }
 };
